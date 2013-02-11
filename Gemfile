11db6903344fc148b5b5ef1d475707ff5d0a050d
Keisuke Ota <arvelt.s@gmail.com>
2013-02-11 21:01:52 +0900

    apache+passenger環境で動作できるように修正。

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sequel'
+gem 'omniauth'
+gem 'omniauth-google-oauth2'
+gem 'sqlite3'
+gem 'slim'
--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 require 'sinatra'
 require "sinatra/reloader" if development?
-require 'Sequel'
+require 'sequel'
 require "omniauth"
 require "omniauth-google-oauth2"
 require "./models/todo"
@@ -9,10 +9,18 @@ require 'slim'
 Slim::Engine.default_options[:pretty] = true  #出力htmlを整形する設定  true=>する,false=>しない
 
 #環境設定
-#set :environment, :production
-set :environment, :development
- 
-# Sinatra のセッションを有効にする
+set :environment, :production
+#set :environment, :development
+
+#静的ファイルの場所を固定持ちさせる
+configure :development do
+  @@root = ''
+end
+configure :production do
+  @@root = '/todolist/'
+end
+
+# Sinatra のセッションを有効にする-
 enable :sessions
   
 # OmniAuth の設定
@@ -34,35 +42,90 @@ configure :development do
 end
 
 get "/auth/:provider/callback" do
+  @root = @@root
+
   # 認証情報は request.env に格納されている
+  # uidとnameをセッションに入れて使う
+  
+  #raise request.env["omniauth.auth"]
   @oauth = request.env["omniauth.auth"]
+  session[:logined] = true
   session[:user_id] = @oauth['uid']
+  session[:name] = @oauth['info']['name']
+  @logined = session[:logined]
+  p @logined
     
-  @todos = Todo.all
-  slim :index
+#  @todos = Todo.all
+#  slim :index
+  configure :development do
+    redirect '/'
+  end
+  configure :production do
+    redirect @@root
+  end
 end
 
 get '/logout' do
-  session.delete("omniauth.auth".to_sym)
-  redirect '/'
+  session.clear
+  configure :development do
+    redirect '/'
+  end
+  configure :production do
+    redirect @@root
+  end
 end
 
 get '/' do
+  @root = @@root
+  
+  p session[:user_id]
+  p session[:session_id]
+  
+  #セッション値を取得
+  @logined = session[:logined]
+  @name = session[:name]
+  user_id = session[:user_id]
+  p @logined
+
+  #------------------------------------------
+  #　Session[user_id]に有効なIDする設定する
+  #   →セッションIDか、auth認証したIDのどちらかが入っている
+  #------------------------------------------
+  user_id = ""
+  @name = session[:name] unless session[:name]
+  if session[:user_id].nil? 
+    user_id = session[:session_id]
+    session[:user_id] =  session[:session_id]
+  else
+    user_id = session[:user_id] 
+    @name = session[:name]
+  end
+  p user_id
+
   
   #Commentsテーブルから結果セットを取得してビューへ渡す
-
-  @todos = Todo.all
+  p request.path_info
+  @todos = Todo.where(:user_id=>user_id).all
   p "test"
   p @todos
   slim :index
 end
 
 post '/add' do
+  @root = @@root
   
+  p session[:user_id]
+  p session[:session_id]
+
   p params[:send_content]
   p params[:send_duedatetime]
 
+  @logined = session[:logined]
+  p @logine
+    
   user_id = session[:user_id]
+  @name = session[:name] unless session[:name]
+  
   due_date = params[:send_duedatetime]
   
   @err = nil
@@ -90,27 +153,36 @@ post '/add' do
     @todo.save
   end
   
-  #全てのtodoを取得して返す
-  @todos = Todo.all
+  #自分のtodoを取得して返す
+  @todos = Todo.where(:user_id=>user_id).all
   slim :_list
 end
 
 post '/delete' do
-  
-  p params[:key]
+  @root = @@root
+ 
   key = params[:key]
+  user_id = session[:user_id]
+  p key
+  p user_id
     
+  user_id = session[:user_id]
+  @name = session[:name] unless session[:name]
+  p user_id
+
+  
   #取得したパラメーターを元にtodoモデルを作成
   @todo = Todo[:id=>key]
   @todo.destroy
   
-  #全てのtodoを取得して返す
-  @todos = Todo.all
+  #自分のtodoを取得して返す
+  @todos = Todo.where(:user_id=>user_id).all
   slim :_list
 end
 
 post '/update' do
-  
+  @root = @@root
+
   p params[:key]
   p params[:status]
   p params[:content]
@@ -121,6 +193,10 @@ post '/update' do
   status =  params[:status]
   content = params[:content]
   due_date = params[:due_date]
+  user_id = session[:user_id]
+
+  user_id = session[:user_id]
+  @name = session[:name] unless session[:name]
 
   @err = nil
 
@@ -151,7 +227,7 @@ post '/update' do
   end
     
   
-  #全てのtodoを取得して返す
-  @todos = Todo.all
+  #自分のtodoを取得して返す
+  @todos = Todo.where(:user_id=>user_id).all
   slim :_list
 end
